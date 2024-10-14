64d22dcf204d6bb14718756728730694e47ebe15
PHAN Xuan Quang <20521008@ms.uit.edu.vn>
2024-10-14 14:32:38 +0700

    done

--- Adobe Camera Profiles Unlocker Neo/MainWindow.xaml.cs
+++ Adobe Camera Profiles Unlocker Neo/MainWindow.xaml.cs
@@ -7,7 +7,6 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.System;
--- Engineer/Updater.cs
+++ Engineer/Updater.cs
@@ -1,9 +1,4 @@
 ﻿using Octokit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Engineer
 {
